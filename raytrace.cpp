cd7bc0092f37cfafdc8fc6cfe8fb556d9d7da19d
kocc <nukozilla@gmail.com>
2023-05-16 12:56:37 +0200

    its shitty, but something happens

--- raytrace.cpp
+++ raytrace.cpp
@@ -2,17 +2,14 @@
 // Computer Graphics Sample Program: Ray-tracing-let
 //=============================================================================================
 #include "framework.h"
-
-struct Material {
-	vec3 ka, kd, ks;
-	float  shininess;
-	Material(vec3 _kd, vec3 _ks, float _shininess) : ka(_kd * M_PI), kd(_kd), ks(_ks) { shininess = _shininess; }
-};
+#include <cmath>
+#include <cstdio>
+#include <math.h>
+#include <vector>
 
 struct Hit {
 	float t;
 	vec3 position, normal;
-	Material * material;
 	Hit() { t = -1; }
 };
 
@@ -22,8 +19,6 @@ struct Ray {
 };
 
 class Intersectable {
-protected:
-	Material * material;
 public:
 	virtual Hit intersect(const Ray& ray) = 0;
 };
@@ -32,10 +27,9 @@ struct Sphere : public Intersectable {
 	vec3 center;
 	float radius;
 
-	Sphere(const vec3& _center, float _radius, Material* _material) {
+	Sphere(const vec3& _center, float _radius) {
 		center = _center;
 		radius = _radius;
-		material = _material;
 	}
 
 	Hit intersect(const Ray& ray) {
@@ -53,10 +47,86 @@ struct Sphere : public Intersectable {
 		hit.t = (t2 > 0) ? t2 : t1;
 		hit.position = ray.start + ray.dir * hit.t;
 		hit.normal = (hit.position - center) * (1.0f / radius);
-		hit.material = material;
 		return hit;
 	}
 };
+
+struct Triangle : Intersectable {
+	vec3 p1, p2, p3;
+
+	Triangle(vec3 p1, vec3 p2, vec3 p3) : p1(p1), p2(p2), p3(p3) {
+	}
+
+	Hit intersect(const Ray& ray) {
+		Hit hit;
+		vec3 norm = cross(p2 - p1, p3 - p1);
+		float t = dot(p1 - ray.start, norm) / dot(ray.dir, norm);
+		vec3 p = ray.start + ray.dir * t;
+		if(dot(cross(p2 - p1, p - p1), norm) > 0 && 
+		dot(cross(p3-p2,p-p2), norm) > 0 && 
+			dot(cross(p1 - p3, p - p3), norm) > 0 ) {
+			hit.t = t;
+			hit.position = ray.start + ray.dir * hit.t;
+		hit.normal = normalize(norm); 
+		}
+	return hit;
+	}
+};
+
+struct Cone : Intersectable {
+	vec3 pos;
+	vec3 norm;
+	float h;
+	float angle;
+
+	Cone(vec3 _pos, vec3 _norm, float _h, float a) {
+		pos = _pos;
+		norm = _norm;
+		h = _h;
+		angle = a;
+	}
+
+	Hit intersect(const Ray& ray) {
+		Hit hit;
+		vec3 A = ray.start - pos;
+		float B = cosf(angle)*cosf(angle);
+		vec3 d = ray.dir;
+		float a = dot(d, norm)*dot(d, norm) - dot(d, d)*B;
+		float b = 2 * (dot(A, norm) * dot(d, norm) - dot(A, d) * B);
+		float c = dot(A, norm) * dot(A, norm) - dot(A, A) * B;
+		float diskriminant = (b*b - 4 * a * c);
+		if(diskriminant < 0) {
+			//printf("elso = %f\nmasodik = %f\nharadik = %f\n", a, b, c);
+		//	printf("disc\n");
+			return hit;
+		}
+		float t1 = (-b + (sqrt(diskriminant))) / (2 * a);
+		float t2 = (-b - (sqrt(diskriminant))) / (2 * a);
+		float vicces1 = dot((A + d*t1), norm);
+		float vicces2 = dot((A + d*t2), norm);
+		if(0 <= vicces1 && vicces1 <= h && t1 > 0) {
+			hit.t = t1;
+			//printf("1es\n");
+			hit.position = ray.start + ray.dir * hit.t;
+			hit.normal = 
+				2 * dot(hit.position - pos, norm) * norm - 2*(hit.position - pos)*B; 
+			normalize(hit.normal);
+			return hit;
+		}
+		if(0 <= vicces2 && vicces2 <= h) {
+			hit.t = t2;
+			//printf("2es\n");
+			hit.position = ray.start + ray.dir * hit.t;
+			hit.normal = 
+				2 * dot(hit.position - pos, norm) * norm - 2*(hit.position - pos)*B; 
+			normalize(hit.normal);
+			return hit;
+		}
+	//	printf("sima\n");
+		return hit;
+	}
+};
+
 
 class Camera {
 	vec3 eye, lookat, right, up;
@@ -76,13 +146,66 @@ public:
 };
 
 struct Light {
-	vec3 direction;
-	vec3 Le;
-	Light(vec3 _direction, vec3 _Le) {
-		direction = normalize(_direction);
-		Le = _Le;
-	}
+	vec3 pos, color;
+
+	Light(vec3 pos, vec3 color) : pos(pos), color(color) {}
 };
+
+std::vector<Triangle> helper(std::vector<vec3> vertices, std::vector<std::vector<int>> indecies, vec3 offset, float scale) {
+	std::vector<Triangle> trigs;
+
+	for(auto xd : indecies) {
+		trigs.emplace_back(scale * vertices[xd[0]] + offset, scale * vertices[xd[1]] + offset, scale * vertices[xd[2]] + offset);
+	}
+	return trigs;
+}
+
+
+
+std::vector<Triangle> cubeMaker() {
+	std::vector<vec3> vertices = {
+		vec3(0.0,  0.0,  0.0),
+		vec3(0.0,  0.0,  1.0),
+		vec3(0.0,  1.0,  0.0),
+		vec3(0.0,  1.0,  1.0),
+		vec3(1.0,  0.0,  0.0),
+		vec3(1.0,  0.0,  1.0),
+		vec3(1.0,  1.0,  0.0),
+		vec3(1.0,  1.0,  1.0),	
+	};
+	
+	std::vector<std::vector<int>> indexes = {
+		{0,  6,  4},
+		{0,  2,  6}, 
+		{0,  3,  2}, 
+		{0,  1,  3}, 
+		{2,  7,  6}, 
+		{2,  3,  7}, 
+		{0,  4,  5}, 
+		{0,  5,  1}, 
+		};
+
+	return helper(vertices, indexes, vec3(-0.5, -0.5, -0.5), 1);
+}
+
+std::vector<Triangle> tetrahedronMaker() {
+	std::vector<vec3> vertices = {
+		vec3(1.00, 1.00, 1.00),
+		vec3(2.00, 1.00, 1.00),
+		vec3(1.00, 2.00, 1.00),
+		vec3(1.00, 1.00, 2.00),
+	};
+
+	std::vector<std::vector<int>> indexes = {
+		{1, 3, 2},
+		{1, 4, 3},
+		{1, 2, 4},
+		{2, 3, 4},
+	};
+
+	return helper(vertices, indexes, vec3(-0.5, -0.5, -0.5), 0.2);
+}
+
 
 float rnd() { return (float)rand() / RAND_MAX; }
 
@@ -95,18 +218,26 @@ class Scene {
 	vec3 La;
 public:
 	void build() {
-		vec3 eye = vec3(0, 0, 2), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
+		vec3 eye = vec3(sqrt(2), 0, sqrt(2)), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
 		float fov = 45 * M_PI / 180;
 		camera.set(eye, lookat, vup, fov);
 
-		La = vec3(0.4f, 0.4f, 0.4f);
-		vec3 lightDirection(1, 1, 1), Le(2, 2, 2);
-		lights.push_back(new Light(lightDirection, Le));
+		La = vec3(0, 0, 0);
+		lights.push_back(new Light(vec3(0, 0, 0), vec3(1, 0, 0)));
 
 		vec3 kd(0.3f, 0.2f, 0.1f), ks(2, 2, 2);
-		Material * material = new Material(kd, ks, 50);
-		for (int i = 0; i < 500; i++) 
-			objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), rnd() * 0.1f, material));
+		//objects.push_back(new Cone(vec3(0, 0, 0), vec3(sqrt(2)/2, 0, -sqrt(2)), 0.2, M_PI/8));
+		//objects.push_back(new Triangle(vec3(0, 0, 0), vec3(1, 0, 0), vec3(0, 1, 0)));
+		for (auto t : cubeMaker()) {
+			objects.push_back(new Triangle(t));
+		}
+		for (auto t : tetrahedronMaker()) {
+			objects.push_back(new Triangle(t));
+		}
+
+
+		//for (int i = 0; i < 150; i++) 
+			//objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), rnd() * 0.1f, material));
 	}
 
 	void render(std::vector<vec4>& image) {
@@ -137,16 +268,13 @@ public:
 	vec3 trace(Ray ray, int depth = 0) {
 		Hit hit = firstIntersect(ray);
 		if (hit.t < 0) return La;
-		vec3 outRadiance = hit.material->ka * La;
-		for (Light * light : lights) {
-			Ray shadowRay(hit.position + hit.normal * epsilon, light->direction);
-			float cosTheta = dot(hit.normal, light->direction);
-			if (cosTheta > 0 && !shadowIntersect(shadowRay)) {	// shadow computation
-				outRadiance = outRadiance + light->Le * hit.material->kd * cosTheta;
-				vec3 halfway = normalize(-ray.dir + light->direction);
-				float cosDelta = dot(hit.normal, halfway);
-				if (cosDelta > 0) outRadiance = outRadiance + light->Le * hit.material->ks * powf(cosDelta, hit.material->shininess);
-			}
+		vec3 outRadiance = vec3(0.2, 0.2, 0.2) * (1 - dot(hit.normal, ray.dir));
+		for(auto light : lights) {
+			Ray shadowray(hit.position, light->pos - hit.position);
+			Hit shadowhit = firstIntersect(shadowray);
+			if (shadowhit.t < 0 || shadowhit.t > length(light->pos - hit.position)) {
+				outRadiance = outRadiance + light->color;
+			} 
 		}
 		return outRadiance;
 	}
@@ -176,7 +304,7 @@ const char *fragmentSource = R"(
 
 	uniform sampler2D textureUnit;
 	in  vec2 texcoord;			// interpolated texture coordinates
-	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocation
+	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocationdisc
 
 	void main() {
 		fragmentColor = texture(textureUnit, texcoord); 
